a7dcd7b95a841704aa07dd6fcf0d146529a35837
ritchey0713 <ritchey0713@gmail.com>
2020-12-19 14:59:28 -0500

    commit tree ea22a18d7e6dbcae4936f850df30c71218f7c785

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,27 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |person, age|
+    puts "Happy Birthday #{person}! You are now #{age} years old!"
+  end 
 end
+
+# passengers = {
+#   suite_a: "Amanda Presley", 
+#   suite_b: "Seymour Hoffman", 
+#   suite_c: "Alfred Tennyson", 
+#   suite_d: "Charlie Chaplin", 
+#   suite_e: "Crumpet the Elf"
+#   }
+
+def select_winner(passengers)
+  winner = ""
+    passengers.each do |room, person|
+      if room == :suite_a && person.start_with?("A")
+        winner << person
+      end 
+    end 
+  winner
+end 
 
 
